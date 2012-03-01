0d70e4de2266e9738fe61fd0a910bc0268fc22bf
Anthony Sottile <asottile@umich.edu>
2012-03-01 01:28:55 +0000

    Added NumberPicker class for KenKen Android
    Java KenKen

---
+++ KenKenAndroid/src/com/anthonysottile/kenken/NumberPicker.java
@@ -0,0 +1,48 @@
+package com.anthonysottile.kenken;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class NumberPicker {
+	
+	private static Random random = new Random();
+	private int length = 0;
+	private int attempted = 0;
+	private List<Integer> collection = new ArrayList<Integer>();
+
+	public int GetRemaining() {
+		return this.length - this.attempted;
+	}
+
+    public int GetNext() throws Exception
+    {
+        if (this.attempted == this.length)
+            throw new Exception("None Left!");
+
+        int lastCardIndex = this.length - this.attempted - 1;
+
+        int cardDrawn = random.nextInt(this.length - this.attempted);
+
+        // Swap the drawn card with the last card in deck
+        int temp = this.collection.get(cardDrawn);
+        collection.set(cardDrawn, collection.get(lastCardIndex));
+        collection.set(lastCardIndex, temp);
+
+        this.attempted++;
+
+        return temp;
+    }
+	
+    public void Reset() {
+    	this.attempted = 0;
+    }
+    
+    public NumberPicker(int n) {
+        for (int i = 1; i <= n; i += 1) {
+            this.collection.add(i);
+        }
+
+        this.length = n;
+    }
+}
