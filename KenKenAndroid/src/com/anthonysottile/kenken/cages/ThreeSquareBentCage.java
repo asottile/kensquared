a7d95c7b8481b3059f7bfa523d12a823b46ed36d
Anthony Sottile <asottile@umich.edu>
2012-03-02 01:40:15 +0000

    Added three square bent cage.
    Java KenKen

---
+++ KenKenAndroid/src/com/anthonysottile/kenken/cages/ThreeSquareBentCage.java
@@ -0,0 +1,151 @@
+package com.anthonysottile.kenken.cages;
+
+import android.graphics.Point;
+
+import com.anthonysottile.kenken.*;
+
+public class ThreeSquareBentCage extends BaseCage {
+	public ThreeSquareBentCage(KenKenGame game, Point location, boolean up, boolean left)
+    {
+        this.signLocation = location;
+
+        Point secondSquare;
+        Point thirdSquare;
+        if (up)
+        {
+            if (left)
+            {
+                secondSquare = Points.add(location, Points.Right);
+                thirdSquare = Points.add(location, Points.Down);
+
+                // +---+---+
+                // | X | 2 |
+                // +---+---+
+                // | 3 |
+                // +---+
+                //
+                // top, left, bottom, right, inner bottom, inner right
+            	this.renderLines.add(new RenderLine(location, 2, true));
+            	this.renderLines.add(new RenderLine(location, 2, false));
+                this.renderLines.add(
+            		new RenderLine(Points.add(location, Points.multiply(2, Points.Down)), 1, true)
+        		);
+                this.renderLines.add(
+            		new RenderLine(Points.add(location, Points.multiply(2, Points.Right)), 1, false)
+        		);
+                this.renderLines.add(
+            		new RenderLine(Points.add(Points.add(location, Points.Down), Points.Right), 1, true)
+        		);
+                this.renderLines.add(
+            		new RenderLine(Points.add(Points.add(location, Points.Down), Points.Right), 1, false)
+        		);
+            }
+            else
+            {
+                secondSquare = Points.add(location, Points.Right);
+                thirdSquare = Points.add(secondSquare, Points.Down);
+
+                // +---+---+
+                // | X | 2 |
+                // +---+---+
+                //     | 3 |
+                //     +---+
+                //
+                // top, left, bottom, right, inner bottom, inner left
+                this.renderLines.add(new RenderLine(location, 2, true));
+                this.renderLines.add(new RenderLine(location, 1, false));
+                this.renderLines.add(
+            		new RenderLine(
+        				Points.add(Points.add(location, Points.multiply(2, Points.Down)), Points.Right),
+        				1, true
+    				)
+        		);
+                this.renderLines.add(
+            		new RenderLine(Points.add(location, Points.multiply(2, Points.Right)), 2, false)
+        		);
+                this.renderLines.add(
+            		new RenderLine(Points.add(location, Points.Down), 1, true)
+        		);
+                this.renderLines.add(
+            		new RenderLine(Points.add(Points.add(location, Points.Down), Points.Right), 1, false)
+        		);
+            }
+        }
+        else
+        {
+            if (left)
+            {
+                secondSquare = Points.add(location, Points.Down);
+                thirdSquare = Points.add(secondSquare, Points.Right);
+
+                // +---+
+                // | X |
+                // +---+---+
+                // | 2 | 3 |
+                // +---+---+
+                //
+                // top, left, bottom, right, inner right, inner top
+            	this.renderLines.add(new RenderLine(location, 1, true));
+            	this.renderLines.add(new RenderLine(location, 2, false));
+            	this.renderLines.add(
+        			new RenderLine(Points.add(location, Points.multiply(2, Points.Down)), 2, true)
+    			);
+            	this.renderLines.add(
+        			new RenderLine(
+    					Points.add(Points.add(location, Points.multiply(2, Points.Right)), Points.Down),
+    					1, false
+					)
+    			);
+            	this.renderLines.add(
+        			new RenderLine(Points.add(location, Points.Right), 1, false)
+    			);
+            	this.renderLines.add(
+        			new RenderLine(Points.add(Points.add(location, Points.Right), Points.Down), 1, true)
+    			);
+            }
+            else
+            {
+                secondSquare = Points.add(location, Points.Down);
+                thirdSquare = Points.add(secondSquare, Points.Left);
+
+                //     +---+
+                //     | X |
+                // +---+---+
+                // | 3 | 2 |
+                // +---+---+
+                //
+                // top, inner left, right, inner top, left, bottom
+                this.renderLines.add(new RenderLine(location, 1, true));
+                this.renderLines.add(new RenderLine(location, 1, false));
+                this.renderLines.add(new RenderLine(Points.add(location, Points.Right), 2, false));
+                this.renderLines.add(
+            		new RenderLine(Points.add(Points.add(location, Points.Down), Points.Left), 1 , true)
+        		);
+                this.renderLines.add(
+            		new RenderLine(Points.add(Points.add(location, Points.Down), Points.Left), 1 , false)
+        		);
+                this.renderLines.add(
+            		new RenderLine(
+        				Points.add(Points.add(location, Points.multiply(2, Points.Down)), Points.Left),
+        				2 , true
+    				)
+        		);
+            }
+        }
+
+        game.setOccupied(location);
+        game.setOccupied(secondSquare);
+        game.setOccupied(thirdSquare);
+
+        this.signNumber =
+            CageGenerator.DetermineSign
+            (
+                new int[]
+                {
+                    game.getLatinSquare().getValues()[location.x][location.y],
+                    game.getLatinSquare().getValues()[secondSquare.x][secondSquare.y],
+                    game.getLatinSquare().getValues()[thirdSquare.x][thirdSquare.y]
+                }
+            );
+    }
+}
