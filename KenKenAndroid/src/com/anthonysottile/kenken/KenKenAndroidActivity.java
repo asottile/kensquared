95646edfec7b185329fd271d3ccc98b43dbaadef
Anthony Sottile <asottile@umich.edu>
2012-03-02 14:37:13 +0000

    Added menu!
    Java KenKen

--- KenKenAndroid/src/com/anthonysottile/kenken/KenKenAndroidActivity.java
+++ KenKenAndroid/src/com/anthonysottile/kenken/KenKenAndroidActivity.java
@@ -2,12 +2,43 @@ package com.anthonysottile.kenken;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class KenKenAndroidActivity extends Activity {
-    /** Called when the activity is first created. */
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.newGame:
+
+                return true;
+            case R.id.preferences:
+
+                return true;
+            case R.id.statistics:
+
+            	return true;
+            case R.id.about:
+
+            	return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
