ee6f67c7cda081654c1af5eba1f92673389eb5e4
Anthony Sottile <asottile@umich.edu>
2012-03-24 18:38:48 +0000

    Added skeleton for Values Layout
    Java KenKen

---
+++ KenKenAndroid/src/com/anthonysottile/kenken/ui/ValuesLayout.java
@@ -0,0 +1,11 @@
+package com.anthonysottile.kenken.ui;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+public class ValuesLayout extends LinearLayout {
+	public ValuesLayout(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+}
